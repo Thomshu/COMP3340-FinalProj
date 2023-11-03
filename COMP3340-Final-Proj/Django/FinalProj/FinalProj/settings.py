e728471423c30475ac67c6d1ea96183c7f09551d
Thomson Ly <thomson.ly98@gmail.com>
2023-11-03 16:38:50 -0400

    Conversion from SQLite to PostgreSQL completed

--- COMP3340-Final-Proj/Django/FinalProj/FinalProj/settings.py
+++ COMP3340-Final-Proj/Django/FinalProj/FinalProj/settings.py
@@ -87,9 +87,13 @@ WSGI_APPLICATION = 'FinalProj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'takcomputersdb',
+        'USER': 'thomshu',
+        'PASSWORD': 'lyt135800',
+        'HOST': 'localhost', # Change this if your PostgreSQL server is on a different host
+        'PORT': '', # Leave empty to use the default port
+        }
 }
 
 
